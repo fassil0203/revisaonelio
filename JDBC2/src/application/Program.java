3bcc38dddcf69d73cdda1fd8860febdeefdc01c9
Fabio <fassil0203@gmail.com>
2024-04-10 20:52:23 -0300

    Inserindo Dados D1 D2

--- JDBC2/src/application/Program.java
+++ JDBC2/src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -16,33 +18,42 @@ public class Program {
 		Connection conn = null;
 		PreparedStatement st = null;
 		try {
-			conn = DB.getConnection();				//Conexao com bco preparando o comando sql
-			
-			st = conn.prepareStatement("INSERT INTO seller"
-			+"(Name,Email,BirthDate,BaseSalary,DepartmentId)"
-		    +"VALUES"
-			+"(?,?,?,?,?)");
-			st.setString(1,"Carll Purple");
-			st.setString(2,"carl@gmail.com");
+			conn = DB.getConnection(); // Conexao com bco preparando o comando sql
+			/*
+			st = conn.prepareStatement(
+					"INSERT INTO seller" + "(Name,Email,BirthDate,BaseSalary,DepartmentId)" + "VALUES" + "(?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, "Carll Purple");
+			st.setString(2, "carl@gmail.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1990").getTime()));
 			st.setDouble(4, 6000.0);
 			st.setInt(5, 4);
-			
-			int rowsAffected = st.executeUpdate ();
-			
-			System.out.println("Done!! Rows Affected:" + rowsAffected);
-		}
-		catch(SQLException e) {
+			*/
+			st = conn.prepareStatement("insert into department (Name) values ('D1'),('D2')",
+					Statement.RETURN_GENERATED_KEYS);
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = "+ id);
+				}
+
+			} else {
+				System.out.println("No rown Affected!");
+			}
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		catch (ParseException e) {
-			e.printStackTrace();
-		}
-		finally {
-			DB.closeStatement(st);		//fechando os recursos que abrimos;
+		//} catch (ParseException e) {
+			//e.printStackTrace();
+		} finally {
+			DB.closeStatement(st); // fechando os recursos que abrimos;
 			DB.closeConnection();
 		}
-		
+
 	}
 
 }
