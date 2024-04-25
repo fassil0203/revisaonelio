a0af00fb11e4d835520f0318dedb4008a3d746fb
Fabio <fassil0203@gmail.com>
2024-04-24 21:33:46 -0300

    Reusing Instatiation

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -8,16 +8,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		/*Department obj = new Department(1,"books");
-		
-		Seller seller = new Seller(21,"Mike Jordan","mj@gmail.com",new Date(),3000.0,obj);
-		
-		System.out.println(obj);
-		System.out.println(seller);
-		System.out.println(seller);*/
-		
 		SellerDao sellerDao = DaoFactory.creatSellerDao();
-		Seller seller =sellerDao.findById(3);
+		
+		System.out.println("===TEST 1: seller findById ===");
+		Seller seller =sellerDao.findById(5);
 		System.out.println(seller);
 
 	}
